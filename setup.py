e172618bb6015b80a685be7273c85fd9bdad4014
Aki Sasaki <aki@escapewindow.com>
2016-06-10 11:22:22 -0700

    requirements-py3.txt

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
 
+from pip.req import parse_requirements
 from setuptools import setup, find_packages
+import os
 import sys
 
 deps = [
@@ -18,8 +20,15 @@ deps = [
 
 if sys.version_info[:2] == (2, 7):
     deps.extend([
+        'poster',
         'Twisted==10.1.0',
     ])
+
+if sys.version_info >= (3, 5):
+    """If running on py3, you need to `pip install -r requirements-py3.txt` after
+    running setup.py, until the dependencies either support py3 or we drop their
+    use."""
+    pass
 
 setup(
     name="buildtools",
